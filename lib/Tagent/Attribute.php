bba13be0185bf1c7d39fafba3b8b5981a5da9b5c
zopezora <zopezora@google.com>
2015-03-09 10:00:44 +0900

    constant PATTERN

--- lib/Tagent/Attribute.php
+++ lib/Tagent/Attribute.php
@@ -4,30 +4,14 @@ namespace Tagent;
 class Attribute
 {
     // const pattern
-    const RESERVED_ATTRS  = 'Module|Pull|Loop|Read|Trim|Parse|Close|Check|Debug|Store|Header|Reopen|Restore|Refresh|Template';
+    const RESERVED_ATTRS    = 'Module|Pull|Loop|Read|Trim|Parse|Close|Check|Debug|Store|Header|Reopen|Restore|Refresh|Template';
+    const ATTRIBUTE_PATTERN = "/(?:[^'\"\s]+|\"(?:\\\\\"|[^\"])*\"|'(?:\\\\'|[^'])*')+/";
+    const VARKEY_PATTERN    = "/^\[(\w+)\]$/";
+    const VALID_PATTERN     = "/(?|(\w+)|(\[\w+\]))=([^'\"\s]+|\"(?:\\\\\"|[^\"])*\"|'(?:\\\\'|[^'])*')/";
     /**
      * @var array
      */
     public $reserved = array();
-
-    // public $reservedQueue = null;
-    // protected $priorityTable = array(
-    //             "Debug"         => 15,
-    //             "Header"        => 14,
-    //             "Store"         => 13,
-    //             "Module"        => 12,
-    //             "Reopen"        => 11,
-    //             "Refresh"       => 10,
-    //             "Pull"          => 9,
-    //             "Loop"          => 8,
-    //             "Template"      => 7,
-    //             "Read"          => 6,
-    //             "Restore"       => 5,
-    //             "Trim"          => 4,
-    //             "Check"         => 3,
-    //             "Parse"         => 2,
-    //             "Close"         => 1,
-    // );
     /**
      * @var array
      */
@@ -44,16 +28,12 @@ class Attribute
      */
     public function __construct($source, ParseResource $resource)
     {
-//        $this->reservedQueue = new \SplPriorityQueue;
-        if (preg_match_all("/(?:[^'\"\s]+|\"(?:\\\\\"|[^\"])*\"|'(?:\\\\'|[^'])*')+/", $source, $matches)) {
+        if (preg_match_all(self::ATTRIBUTE_PATTERN, $source, $matches)) {
             $array = $matches[0];
-            $valid_pattern    = "/(?|(\w+)|(\[\w+\]))=([^'\"\s]+|\"(?:\\\\\"|[^\"])*\"|'(?:\\\\'|[^'])*')/";
-
             $reserved_pattern = "/^(".self::RESERVED_ATTRS.")$/";
-            $varkey_pattern   = "/^\[(\w+)\]$/i";
             foreach($array as $v) {
                 // valid attribute
-                if (preg_match($valid_pattern, $v, $sp_match)){
+                if (preg_match(self::VALID_PATTERN, $v, $sp_match)){
                     $key   = $sp_match[1];   // foo or [foo]
                     $value = $sp_match[2];   // 'bar' or {@name}
 
@@ -63,17 +43,14 @@ class Attribute
                         // un quate value, try for fetch {@VARIABLE}
                         $value = $resource->varFetch($value, true);
                     }
-                    // sepalate reserved attribute
-                    $parentkey = 'params';
+                    // separate reserved appends params
                     if (preg_match($reserved_pattern, $key, $attr_match)){
                         $this->reserved[]= array($key, $value);
-//                        $this->reservedQueue->insert(array($key, $value), $this->priorityTable[$key]);
-                    } else {
-                        if (preg_match($varkey_pattern, $key, $varkey_match)) {
-                            $parentkey = 'appends';
+                    } elseif (preg_match(self::VARKEY_PATTERN, $key, $varkey_match)) {
                             $key = $varkey_match[1];
-                        }
-                        $this->{$parentkey}[$key] = $value;
+                            $this->appends[$key] = $value;
+                    } else {
+                            $this->params[$key] = $value;
                     }
                 } else {
                     // Unvalid attribute
@@ -81,6 +58,6 @@ class Attribute
                     $agent->log(E_WARNING, "Unvalid attribute (".$v.")", true, $resource->module);
                 }
             } // end of foreach
-        }
+        } // end of match_all
     }
 }
