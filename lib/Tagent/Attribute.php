b2744fc61bc61f39a71bc671f603f1c4d29ff687
zopezora <zopezora@google.com>
2015-03-15 13:56:59 +0900

    change Valid pattern for quoted index

--- lib/Tagent/Attribute.php
+++ lib/Tagent/Attribute.php
@@ -12,7 +12,7 @@ class Attribute
     // const pattern
     const RESERVED_PATTERN  = "/^(Module|Pull|Loop|Read|Trim|Parse|Close|Check|Debug|Store|Header|Reopen|Restore|Refresh|Template)$/";
     const ATTRIBUTE_PATTERN = "/(?:[^'\"\s]+|\"(?:\\\\\"|[^\"])*\"|'(?:\\\\'|[^'])*')+/";
-    const VALID_PATTERN     = "/(?|(\w+)|(\[\w+\]))=([^'\"\s]+|\"(?:\\\\\"|[^\"])*\"|'(?:\\\\'|[^'])*')/";
+    const VALID_PATTERN     = "/(?|(\w+)|(\[\w+\]))=((?:[^'\"\s]+|\"(?:\\\\\"|[^\"])*\"|'(?:\\\\'|[^'])*')+)/";
     /**
      * @var array Parsed result of reserved attributes.
      */
